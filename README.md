7ea0a141b1a36e790fd1355c39097ddf64521cf8
NastyaMittseva <nastya_mittseva@mail.ru>
2023-05-16 19:03:35 +0300

    update readme

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
 # MoVQGAN
 
 [![Framework: PyTorch](https://img.shields.io/badge/Framework-PyTorch-orange.svg)](https://pytorch.org/) [![Huggingface space](https://img.shields.io/badge/🤗-Huggingface-yello.svg)](https://huggingface.co/ai-forever/MoVQGAN)
+[![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)]()
 
 [Habr post]()
 
 `pip install "git+https://github.com/ai-forever/MoVQGAN.git"`
+
+# Models
++ [67M MoVQGAN](https://huggingface.co/ai-forever/MoVQGAN/movqgan_67M.ckpt)
++ [102M MoVQGAN](https://huggingface.co/ai-forever/MoVQGAN/movqgan_102M.ckpt)
++ [270M MoVQGAN](https://huggingface.co/ai-forever/MoVQGAN/movqgan_270M.ckpt)
+
+## How to use:
+
+### Train
+```
+python main.py --config configs/movqgan_270M.yaml
+```
+### Inference
+Check jupyter notebook with example in `./notebooks` folder or [![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)]()
--- main.py
+++ main.py
@@ -1,34 +1,43 @@
 import pytorch_lightning as pl
 from omegaconf import OmegaConf
+from argparse import ArgumentParser
 import torch
 import os
 from movqgan.data.dataset import LightningDataModule
-from pytorch_lightning.loggers import TensorBoardLogger
 from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor
 from pytorch_lightning.loggers import WandbLogger
 from movqgan.util import instantiate_from_config
 import movqgan
 
+
 os.environ["WANDB_API_KEY"] = ""
 os.environ["WANDB_MODE"] = "online"
 
-config = OmegaConf.load('./configs/movqgan_270M.yaml')
 
-model = instantiate_from_config(config['model'])
-data = LightningDataModule(config['data']['train'])
+def run_train(config):
+    model = instantiate_from_config(config['model'])
+    data = LightningDataModule(config['data']['train'])
 
-if os.path.exists(config['ModelCheckpoint']['dirpath']) == False:
-    os.makedirs(config['ModelCheckpoint']['dirpath'])
-callbacks = [
-        ModelCheckpoint(**config['ModelCheckpoint']),
-        LearningRateMonitor(logging_interval='step')
-]
+    if os.path.exists(config['ModelCheckpoint']['dirpath']) == False:
+        os.makedirs(config['ModelCheckpoint']['dirpath'])
+    callbacks = [
+            ModelCheckpoint(**config['ModelCheckpoint']),
+            LearningRateMonitor(logging_interval='step')
+    ]
 
-wandb_logger = WandbLogger(project=config['wandb_project_name'])
-trainer = pl.Trainer(logger=wandb_logger, callbacks=callbacks, **config['trainer'])
+    wandb_logger = WandbLogger(project=config['wandb_project_name'])
+    trainer = pl.Trainer(logger=wandb_logger, callbacks=callbacks, **config['trainer'])
 
-if config['ckpt_path'] == '':
-    ckpt_path = None
-else:
-    ckpt_path = config['ckpt_path']
-trainer.fit(model, data, ckpt_path=ckpt_path)
+    if config['ckpt_path'] == '':
+        ckpt_path = None
+    else:
+        ckpt_path = config['ckpt_path']
+    trainer.fit(model, data, ckpt_path=ckpt_path)
+
+    
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('--config', type=str, help='config path')
+    args = parser.parse_args()
+    config = OmegaConf.load(args.config)
+    run_train(config)
